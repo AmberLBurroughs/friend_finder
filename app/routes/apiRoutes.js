5bf7d242b4ede65bac3f4a377d1935a82293405f
Amber Burroughs <amberlburroughs@gmail.com>
2017-10-27 09:25:17 -0700

    fix game logic

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -13,31 +13,39 @@ module.exports = function(app) {
   app.post("/api/quiz", function(req, res) {
     var index = Math.floor(Math.random() * 10) + 1  
   	console.log("format: ",req.body);
-    var currenFriend = req.body;
+    var newFriend = req.body;
     if(friendsData.length === 1){
-      friendsData.push(req.body);
-      var result = [friendsData[0], sweetsData[index], currenFriend]
+      friendsData.push(newFriend);
+      var result = [friendsData[0], sweetsData[index], currentFriend]
       res.json(result);
     }else {
       console.log(friendsData);
         
-        var current_diff = 0;
-        var lowest_diff = 0;
-        friendsData.forEach(function(friend){
-          friend["scores"].forEach(function(score){
+       
+        var lowestDiff = 1000; // set this to a high number to start.
+        var matchedFriend;
+        var scoresLength = newFriend.scores.length
 
-          })
-        })
+        for(var fr=0;fr<friendsData.length;fr++){
+          var currentFriend=friendsData[fr];
+          var currentDiff = 0;
+          
+          for(var i = 0; i<scoresLength;i++){
+            var scoreA = newFriend.scores[i];
+            var scoreB = currentFriend.scores[i];
+            var diff = Math.abs(scoreA-scoreB);
+            currentDiff += diff;
 
-        /*
+            if(currentDiff < lowestDiff || matchedFriend == undefined){
+              lowestDiff = currentDiff;
+              matchedFriend = currentFriend;
+            }
+          }
 
-        loop though array of objects compare scores current score against each object score
-        
-        difference var start 0 take the absolute from every inex and add to diff var.
-
-      */
-
-
-    }
+        }
+        friendsData.push(newFriend);
+        var result = [matchedFriend, sweetsData[index], newFriend]
+        res.json(result);
+      }
   });
 }
