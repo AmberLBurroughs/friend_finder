970ef67a828c4f938ee63c6e667d876867024410
Amber Burroughs <amberlburroughs@gmail.com>
2018-02-26 10:11:48 -0800

    update server

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,9 +3,10 @@ var sweetsData = require("../data/sweetsData");
 console.log("test: ",friendsData);
 module.exports = function(app) {
 
-	app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
+	app.get("/api/friends", function(req, response) {
+    response.json(friendsData);
   });
+
 	app.get("/api/sweets", function(req, res) {
     res.json(sweetsData);
   });
@@ -13,6 +14,7 @@ module.exports = function(app) {
   app.post("/api/quiz", function(req, res) {
     var index = Math.floor(Math.random() * 10); 
   	console.log("format: ",req.body);
+
     var newFriend = req.body;
     if(friendsData.length === 1){
       friendsData.push(newFriend);
@@ -22,12 +24,16 @@ module.exports = function(app) {
       console.log(friendsData);
       console.log(newFriend);
        
-        var lowestDiff = 1000; // set this to a high number to start.
-        var matchedFriend;
+
+        var lowestDiff = 1000; // set this to any really high number to start.
+
+        var matchedFriend; // undefined becasue initially the friend has not been matched with anyone.
         var scoresLength = newFriend["scores[]"].length
-        for(var fr=0;fr<friendsData.length;fr++){
+        console.log(scoresLength);
+
+        for(var j=0;j<friendsData.length;j++){
           //console.log(": ",currentDiff);
-          var currentFriend=friendsData[fr];
+          var currentFriend=friendsData[j];
           var currentDiff = 0;
           
           for(var i = 0; i<scoresLength;i++){
@@ -38,7 +44,7 @@ module.exports = function(app) {
           }
 
           if(currentDiff < lowestDiff || matchedFriend == undefined){
-            lowestDiff = currentDiff;
+            lowestDiff = currentDiff; 
             matchedFriend = currentFriend;
           }
         }
